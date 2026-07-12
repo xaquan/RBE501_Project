fea0844e4b956398b4223b4f13df77016a5334e5
Xa Quan <quan.h.xa@gmail.com>
2026-07-11 21:48:54 -0700

    Added object detection, transfering object position for robot controller

--- controllers/camera_controller/camera_controller.py
+++ controllers/camera_controller/camera_controller.py
@@ -1,15 +1,36 @@
 from controller import Robot
+import struct
 
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
+# supervisor = Supervisor()
 
-camera = robot.getDevice("top_camera")
+camera = robot.getDevice("overhead_camera")
 camera.enable(timestep)
+camera.recognitionEnable(timestep)
 
-print("Camera Loading...")
+camPos = [0, 0, 1.8]
+
+emitter = robot.getDevice("serial_emitter")
+CAMERA_HEIGHT = 1.8
+
+print("Camera Loaded...")
 
 while robot.step(timestep) != -1:
     image = camera.getImage()
+    objects = camera.getRecognitionObjects()
+    
+    for obj in objects:
+        obj_id = obj.getId()
+        # position is relative to the CAMERA's coordinate frame, not world!
+        cam_x, cam_y, cam_z = obj.getPosition()
 
-    if image is not None:
-        pass
+        # Camera points straight down (rotated 180° about X), mounted at
+        # world height CAMERA_HEIGHT above the origin plane. Convert:
+        world_x = cam_x
+        world_y = -cam_y   # sign flips depending on your camera's rotation axis
+        world_z = -cam_z + CAMERA_HEIGHT
+
+        message = struct.pack('i3f', obj_id, world_x, world_y, world_z)
+        emitter.send(message)
+        # print(f"Object {obj_id} at world pos: ({world_x:.3f}, {world_y:.3f}, {world_z:.3f})")
--- controllers/robot_controller/robot_controller.py
+++ controllers/robot_controller/robot_controller.py
@@ -1,9 +1,15 @@
-from controller import Robot
+from controller import Robot, Supervisor
 import math
-
+from typing import cast
+import struct
 
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
+
+receiver = robot.getDevice("serial_receiver")
+receiver.setChannel(1)  # matches the Emitter above
+receiver.enable(timestep)
+
 
 joint_names = [
     "shoulder_pan_joint",
@@ -29,10 +35,10 @@ for name in joint_names:
 # Initial pose in radians
 initial_position = [
     0.0,
-    -math.pi / 2,
     math.pi / 2,
     -math.pi / 2,
-    -math.pi / 2,
+    math.pi / 2,
+    math.pi / 2,
     0.0,
 ]
 
@@ -41,6 +47,10 @@ for motor, position in zip(motors, initial_position):
 
 
 start_time = robot.getTime()
+
+print("Robot controller loaded.")
+
+detected_object = {}
 
 while robot.step(timestep) != -1:
     elapsed_time = robot.getTime() - start_time
@@ -55,3 +65,17 @@ while robot.step(timestep) != -1:
 
     motors[1].setPosition(shoulder_position)
     motors[2].setPosition(elbow_position)
+    
+    if receiver.getQueueLength() > 0:
+        detected_object = {}
+        
+    while receiver.getQueueLength() > 0:
+        data = receiver.getBytes()
+        obj_id, x, y, z = struct.unpack('i3f', data)
+        receiver.nextPacket()
+
+        # print(f"Received object {obj_id} at ({x:.3f}, {y:.3f}, {z:.3f})")
+        detected_object[obj_id] = [x, y, z]
+        # target_pose = [x, y, z]
+        # joint_angles = your_ik_solver(target_pose)
+        # move arm to joint_angles   
